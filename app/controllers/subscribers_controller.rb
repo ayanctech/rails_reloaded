3b2d1a7c159690f945091d600330c74ace374fb8
Ayan <ayan.chowdhury2050@gmail.com>
2019-07-30 07:43:33 +0530

    re-worked test cases

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -4,34 +4,47 @@ class SubscribersController < ApplicationController
   end
 
   def download_pdf
-    @subscribers=Subscriber.all.order("name ASC")
-    respond_to do |format|
-      format.pdf do
-        pdf = PdfCreator.new(@subscribers)
-        send_data pdf.render,
-          filename: "subscribers-#{Date.today}.pdf",
-          type: "application/pdf",
-          disposition: "inline"
+    if Subscriber.count < 50
+      redirect_to (root_path), flash: { error: "Not Enough Database values ! Seed the DB first"}
+      Rails.application.load_seed
+    else
+      @subscribers=Subscriber.all.order("name ASC")
+      respond_to do |format|
+        format.pdf do
+          pdf = PdfCreator.new(@subscribers)
+          send_data pdf.render,
+            filename: "subscribers-#{Date.today}.pdf",
+            type: "application/pdf",
+            disposition: "inline"
+        end
       end
     end
   end
 
   def download_csv
-    @subscribers=Subscriber.all.order("name ASC")
-
-    respond_to do |format|
-      format.html
-      format.csv { send_data @subscribers.to_csv, filename: "subscribers-#{Date.today}.csv" }
+    if Subscriber.count < 50
+      redirect_to (root_path), flash: { error: "Not Enough Database values ! Seed the DB first"}
+      Rails.application.load_seed
+    else
+      @subscribers=Subscriber.all.order("name ASC")
+      respond_to do |format|
+        format.html
+        format.csv { send_data @subscribers.to_csv, filename: "subscribers-#{Date.today}.csv" }
+      end
     end
   end
 
   def send_mail
-    @subscribers=Subscriber.all.order("name ASC")
-
-    if AdminMailer.data_table(@subscribers).deliver
-      redirect_to (root_path), flash: { notice: "Mail sent"}
+    if Subscriber.count < 50
+      redirect_to (root_path), flash: { error: "Not Enough Database values ! Seed the DB first"}
+      Rails.application.load_seed
     else
-      redirect_to (root_path), flash: { error: "Mail sending error"}
+      @subscribers=Subscriber.all.order("name ASC")
+      if AdminMailer.data_table(@subscribers).deliver
+        redirect_to (root_path), flash: { notice: "Mail sent"}
+      else
+        redirect_to (root_path), flash: { error: "Mail sending error"}
+      end
     end
   end
 
